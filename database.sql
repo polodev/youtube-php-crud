af166612f633fc9186d5f38cc9accc8774c212e8
Polo dev <polodev10@gmail.com>
2017-11-17 12:20:59 +0600

    update readme.md

--- database.sql
+++
@@ -1,8 +0,0 @@
-create database company;
-use company;
-
-create table people (
-  id  int(11) auto_increment primary key,
-  name varchar(30) not null,
-  email varchar(30) not null
-);
--- delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'db.php';
-$id = $_GET['id'];
-$sql = 'DELETE FROM people WHERE id=:id';
-$statement = $connection->prepare($sql);
-if ($statement->execute([':id' => $id])) {
-  header("Location: /");
-}
